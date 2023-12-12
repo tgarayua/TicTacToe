c73c2959742db815a778d195dd61b6b5902ac1eb
Thomas Garayua <tgarayua@gmail.com>
2023-12-12 00:52:12 -0500

    created UI for tic tac toe

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { index in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.red).opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 15,
+                                       height: geometry.size.width/3 - 15)
+                            
+                            Image(systemName: moves[index]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            moves[index] = Move(player: isHumansTurn ? .human : .computer, boardIndex: index)
+                            isHumansTurn.toggle()
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
