dc002f77a1848dd11cb6fbc3e5849cde7cd18886
Thomas Garayua <tgarayua@gmail.com>
2023-12-12 00:30:55 -0500

    first commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Thomas Garayua on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
