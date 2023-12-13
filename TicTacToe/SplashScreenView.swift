c03fbcc8f33f0d66566f5afbab0fa2a184b14958
Thomas Garayua <tgarayua@gmail.com>
2023-12-13 00:49:11 -0500

    added background images for light/dark mode, new PNG logo

--- TicTacToe/SplashScreenView.swift
+++ TicTacToe/SplashScreenView.swift
@@ -8,40 +8,39 @@
 import SwiftUI
 
 struct SplashScreenView: View {
-
+    
     @Environment(\.colorScheme) var colorScheme
     @State private var isActive = false
-    @State private var size = 0.8
-    @State private var opacity = 0.5
     
     var body: some View {
-        if isActive {
-            GameView()
-        } else {
-            VStack {
-                VStack {
-                    Image("TicTacLogo")
-                        .font(.system(size: 80))
-                        .foregroundColor(.red)
-                    Text("Tic Tac Toe")
-                        .font(Font.custom("Baskerville-Bold", size: 26))
-                        .foregroundColor(colorScheme == .dark ? .white : .black)
-                        .background(colorScheme == .dark ? .black : .white)
+        ZStack {
+            if self.isActive {
+                GameView()
+            } else {
+                if colorScheme == .dark {
+                    Image("light mode sky")
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                } else {
+                    Image("darkModeSky")
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
                 }
-                .scaleEffect(size)
-                .opacity(opacity)
-                .onAppear {
-                    withAnimation(.easeIn(duration: 1.2)) {
-                        self.size = 0.9
-                        self.opacity = 1.0
-                    }
+                
+                VStack {
+                    Image("TicTacToeLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
                 }
             }
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                    withAnimation {
-                        self.isActive = true
-                    }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
                 }
             }
         }
